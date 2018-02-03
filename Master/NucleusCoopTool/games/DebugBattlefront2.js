d95fd3db774ed673da94c4b0a55553aeb854ebd8
Qbsoon <ramsters110@gmail.com>
2018-02-03 14:23:10 +0100

    DebugBattlefront2.js things fixed - description
    After adding original file to games folder nucleus couldn't start for me.
    After editing (based on working black ops file) nucleus starts. I can add game to list.
    But it couldn't find gamepads. Now, after editing, it finds.
    Game can be started, but it only starts one instance.

--- Master/NucleusCoopTool/games/DebugBattlefront2.js
+++ Master/NucleusCoopTool/games/DebugBattlefront2.js
@@ -1,12 +1,13 @@
 Game.AddOption("Keyboard Player",
     "The player that will be playing on keyboard and mouse (if any)",
-    Nucleus.KeyboardPlayer.NoKeyboardPlayer,
-    "KeyboardPlayer");
+	"KeyboardPlayer",
+    Nucleus.KeyboardPlayer.NoKeyboardPlayer);
 
 Game.Debug = true;
 Game.SymlinkExe = false;
 Game.SymlinkGame = true;
 Game.SupportsKeyboard = true;
+Game.HandlerInterval = 100;
 Game.ExecutableName = "battlefrontii.exe";
 Game.SteamID = "6060";
 Game.GUID = "6060";
@@ -14,15 +15,22 @@ Game.GameName = "Star Wars: Battlefront 2";
 Game.MaxPlayers = 4;
 Game.MaxPlayersOneMonitor = 4;
 Game.BinariesFolder = "";
-Game.NeedsSteamEmulation = true;
-Game.LauncherTitle = "";
+Game.NeedsSteamEmulation = false;
+Game.LauncherTitle = "Star Wars Battlefront II";
 Game.SaveType = Nucleus.SaveType.None;
 Game.SupportsPositioning = true;
 Game.HideTaskbar = false;
 Game.CustomXinput = true;
 Game.StartArguments = "/win";
 Game.HookNeeded = true;
+Game.Hook.ForceFocus = false;
 Game.HookGameWindowName = "Star Wars Battlefront II";
+Game.Hook.DInputEnabled = false;
+Game.Hook.XInputEnabled = true;
+Game.Hook.XInputReroute = false;
+Game.Hook.BlockMouseEvents = false;
+Game.Hook.BlockKeyboardEvents = false;
+Game.Hook.BlockInputEvents = false;
 Game.LauncherExe = "";
 
 Game.Play = function () {
